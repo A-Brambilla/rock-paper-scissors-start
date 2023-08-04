4b538487888e5738424fec4394c84fd6ad8443f2
A-Brambilla <austin.brambilla@outlook.com>
2023-08-04 04:07:34 +0000

    Very messy win loss statements

--- main.py
+++ main.py
@@ -35,3 +35,24 @@ print(choices[player_choice])
 
 cpu_choice = random.randint(0,2)
 print(choices[cpu_choice])
+
+if player_choice == 0 and cpu_choice == 1:
+  print("You lose!")
+elif player_choice == 0 and cpu_choice == 2:
+  print("You win!")
+elif player_choice == 0 and cpu_choice == 0:
+  print("You draw!")
+elif player_choice == 1 and cpu_choice == 0:
+  print("You win!")
+elif player_choice == 1 and cpu_choice == 1:
+  print("You draw!")
+elif player_choice == 1 and cpu_choice == 2:
+  print("You lose!")
+elif player_choice == 2 and cpu_choice == 0:
+  print("You lose!")
+elif player_choice == 2 and cpu_choice == 1:
+  print("You win!")
+elif player_choice == 2 and cpu_choice == 2:
+  print("You draw!")
+else:
+  print("invalid choice I guess")
